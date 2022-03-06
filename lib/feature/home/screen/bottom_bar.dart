1442803291e7e3872beec21b4ee65fa18493970f
y-alkhateeb <eng.y.alkhateeb@gmail.com>
2022-03-07 02:13:21 +0300

    add overly

--- lib/feature/home/screen/bottom_bar.dart
+++ lib/feature/home/screen/bottom_bar.dart
@@ -27,7 +27,53 @@ class _BottomBarState extends State<BottomBar> with SingleTickerProviderStateMix
     if(mounted)
       setState(() {});
   }
+  void _showOverlay(BuildContext context) async {
 
+    // Declaring and Initializing OverlayState
+    // and OverlayEntry objects
+    OverlayState? overlayState = Overlay.of(context);
+    OverlayEntry overlayEntry;
+    overlayEntry = OverlayEntry(
+        builder: (context) {
+
+      // You can return any widget you like
+      // here to be displayed on the Overlay
+      return Positioned(
+        left: MediaQuery.of(context).size.width * 0.2,
+        top: MediaQuery.of(context).size.height * 0.3,
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Stack(
+            children: [
+              FlutterLogo(),
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.13,
+                left: MediaQuery.of(context).size.width * 0.13,
+                child: Material(
+                  color: Colors.amber,
+                  child: Text(
+                    'I will disappear in 3 seconds.',
+                    style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.height * 0.025,
+                        color: Colors.green),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+
+    // Inserting the OverlayEntry into the Overlay
+    overlayState!.insert(overlayEntry);
+
+    // Awaiting for 3 seconds
+    await Future.delayed(Duration(seconds: 3));
+
+    // Removing the OverlayEntry from the Overlay
+    overlayEntry.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,9 +90,12 @@ class _BottomBarState extends State<BottomBar> with SingleTickerProviderStateMix
               iconSize: 30.0,
               icon: Icon(Icons.home),
               onPressed: () {
-                setState(() {
-                  tabControl.animateTo(0);
-                });
+                // calling the _showOverlay method
+                // when Button is pressed
+                _showOverlay(context);
+                // setState(() {
+                //   tabControl.animateTo(0);
+                // });
               },
             ),
             IconButton(
